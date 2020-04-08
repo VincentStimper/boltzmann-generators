d0d620b8ef61200f95dc467e93ee4e631829fad2
Vincent Stimper <vincent.stimper@gmail.com>
2020-04-08 15:22:45 +0100

    Forward and backward pass switched

--- boltzgen/flows.py
+++ boltzgen/flows.py
@@ -5,6 +5,9 @@ class CoordinateTransform(nf.flows.Flow):
     """
     Coordinate transform for Boltzmann generators, see
     https://science.sciencemag.org/content/365/6457/eaaw1147
+
+    Meaning of forward and backward pass are switched to meet
+    convention of normflow package
     """
     def __init__(self, data, n_dim, z_matrix, backbone_indices):
         """
@@ -19,9 +22,9 @@ class CoordinateTransform(nf.flows.Flow):
         self.mixed_transform = mixed.MixedTransform(n_dim, backbone_indices, z_matrix, data)
 
     def forward(self, z):
-        z_, log_det = self.mixed_transform.forward(z)
+        z_, log_det = self.mixed_transform.inverse(z)
         return z_, log_det
 
     def inverse(self, z):
-        z_, log_det = self.mixed_transform.inverse(z)
+        z_, log_det = self.mixed_transform.forward(z)
         return z_, log_det
