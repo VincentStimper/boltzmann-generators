80fd2209284682c5e1b634d86977f75ef7615470
Vincent Stimper <vincent.stimper@gmail.com>
2020-05-19 15:25:41 +0100

    Made package installable

---
+++ boltzgen/__init__.py
@@ -0,0 +1,9 @@
+from .core import *
+from . import flows
+from . import distributions
+from . import mixed
+from . import internal
+from . import openmm_interface
+from . import pca
+
+__version__ = '0.1'
