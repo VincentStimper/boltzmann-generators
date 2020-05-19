f833e1ce32ec225673881528420764353a5bbdd4
Vincent Stimper <vincent.stimper@gmail.com>
2020-05-19 16:28:40 +0100

    Added function to get torch tensor form trajectory

--- boltzgen/utils.py
+++ boltzgen/utils.py
@@ -1,3 +1,4 @@
+import torch
 import yaml
 
 def get_config(file_path):
@@ -9,3 +10,27 @@ def get_config(file_path):
 
     with open(file_path, 'r') as stream:
         return yaml.load(stream, yaml.FullLoader)
+
+
+def get_coord(traj):
+    """
+    Get coordinates from trajectory as torch tensor
+    :param traj: Openmm trajectory
+    :return: Torch tensor with coordinates
+    """
+
+    traj.center_coordinates()
+
+    # superpose on the backbone
+    ind = traj.top.select("backbone")
+
+    traj.superpose(traj, 0, atom_indices=ind, ref_atom_indices=ind)
+
+    # Gather the training data into a pytorch Tensor with the right shape
+    coord_np = traj.xyz
+    n_atoms = coord_np.shape[1]
+    n_dim = n_atoms * 3
+    coord_np = coord_np.reshape(-1, n_dim)
+    coord = torch.from_numpy(coord_np.astype("float64"))
+
+    return coord
